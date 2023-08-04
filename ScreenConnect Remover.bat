8402051676e646ab4ff3ec158e26d5e542fd19a5
derpyranbowpop <55472010+derpyranbowpop@users.noreply.github.com>
2023-08-04 18:48:17 +1000

    Update ScreenConnect Remover.bat
    Added auto admin

--- ScreenConnect Remover.bat
+++ ScreenConnect Remover.bat
@@ -1,14 +1,20 @@
 @echo off
 set userProfile=%userprofile%
 set "ScreenConnect=%userProfile%\AppData\Local\Apps\2.0"
+set "params=%*"
 
+@rem Auto admin
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+@rem Check if AppData\Local\Apps\2.0 exists 
 if exist "%ScreenConnect%" (
   echo ScreenConnect is installed
   echo Stoping ScreenConnect 
   taskkill /f /im ScreenConnect.WindowsClient.exe
   taskkill /f /im ScreenConnect.ClientService.exe
   echo Deleteing ScreenConnect
-  RD /s "%userProfile%\AppData\Local\Apps"
+  @rem Deletes the folder it is located
+  RD /s /q "%userProfile%\AppData\Local\Apps"
 ) else (
   echo ScreenConnect is NOT installed
 )
